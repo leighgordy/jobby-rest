61160bc5299a799b10cd528d10e2ef6f393cb2cf
leighgordy <leighgordy@gmail.com>
2021-01-17 14:22:19 +0000

    Added basic lint config to project.

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  plugins: ['jest'],
+  env: {
+    amd: true,
+    'jest/globals': true
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 12
+  },
+  rules: {
+    indent: [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    quotes: [
+      'error',
+      'single'
+    ],
+    semi: [
+      'error',
+      'always'
+    ]
+  }
+};
