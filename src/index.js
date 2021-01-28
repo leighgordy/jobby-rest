21a43e336e8c968be3be803ffa954b8ffe9fd471
leighgordy <leighgordy@gmail.com>
2021-01-28 19:01:33 +0000

    Added path in service config which can be overridden by other projects. Moved swagger configuration.

--- src/index.js
+++ src/index.js
@@ -1,30 +1,8 @@
 const express = require('express');
 const service = require('./service-config');
 const app = express();
-const swaggerJsdoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
 
 const port = 3000;
-
-const options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Jobby API',
-      version: '1.0.0',
-      description:
-        'Dummy API that will let me experiment with frontend frameworks',
-    },
-  },
-  apis: ['./src/routes/*.js'],
-};
-
-const specs = swaggerJsdoc(options);
-app.use(
-  '/api-docs',
-  swaggerUi.serve,
-  swaggerUi.setup(specs)
-);
 
 service(app);
 
--- src/service-config.js
+++ src/service-config.js
@@ -1,8 +1,28 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 const jobs = require('./routes/jobs');
 const errorHandler  = require('./error-handler');
 
-module.exports = (app) => {
-  app.use('/jobs', jobs);
+const specs = swaggerJsdoc({
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Jobby API',
+      version: '1.0.0',
+      description:
+        'Dummy API that will let me experiment with frontend frameworks',
+    },
+  },
+  apis: ['./src/routes/*.js'],
+});
+
+module.exports = (app, path = '') => {
+  app.use(
+    '/api-docs',
+    swaggerUi.serve,
+    swaggerUi.setup(specs)
+  );
+  app.use(`${path}/jobs`, jobs);
   app.use(errorHandler);
   return app;
 };
